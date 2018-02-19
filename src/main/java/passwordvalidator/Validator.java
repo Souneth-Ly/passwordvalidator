2f041cd168f871edf8f007b2b6ce48653e020ee6
uonsongly <uonsongly@gmail.com>
2018-02-18 22:46:02 -0700

    add javadoc

--- src/main/java/passwordvalidator/Validator.java
+++ src/main/java/passwordvalidator/Validator.java
@@ -1,9 +1,20 @@
 package passwordvalidator;
 
+/**
+ * Password Validator based on the following rules:
+ * 1. Must be between 5 and 12 characters in length.
+ * 2.  Must consist of a mixture of lowercase letters and numerical digits only, with at least one of each.
+ * 3. Must not contain any sequence of characters immediately followed by the same sequence.
+ */
 public class Validator {
     private boolean foundAlphabet = false;
     private boolean foundDigit = false;
 
+    /**
+     * Test password with all 3 rules.
+     * @param password Password to validate.
+     * @return true for valid password, false otherwise.
+     */
     public boolean isPasswordValid (String password) {
         // Rule #1: Must be non-null, between 5 and 12 characters in length.
        if ( !isLengthValid(password) ) {
@@ -26,10 +37,20 @@ public class Validator {
         return foundAlphabet && foundDigit;
     }
 
+    /**
+     * Check null and length of the password.
+     * @param password Password to validate.
+     * @return true for non-null and valid length, false otherwise.
+     */
     private boolean isLengthValid(String password) {
         return !(password == null || password.length() < 5 || password.length() > 12);
     }
 
+    /**
+     * Check if symbol is letter and lowercase.
+     * @param symbol Character symbol to validate.
+     * @return true for alphabet, false otherwise.
+     */
     private boolean isAlphabetAndLowerCase(char symbol) {
         if(Character.isLetter(symbol) && Character.isLowerCase(symbol)) {
             // an alphabet was found
@@ -39,6 +60,11 @@ public class Validator {
         return false;
     }
 
+    /**
+     * Check if symbol is a number.
+     * @param symbol Character symbol to validate.
+     * @return true for number, false otherwise.
+     */
     private boolean isDigit(char symbol){
         if(Character.isDigit(symbol)) {
             // a digit was found
@@ -48,6 +74,12 @@ public class Validator {
         return false;
     }
 
+    /**
+     * Check repeated sequences in the password.
+     * @param password Password to validate.
+     * @param index Password string index.
+     * @return true for valid for non-repeated sequence, false otherwise.
+     */
     private boolean isValidsequence(String password, int index) {
         if(index > 0 && password.charAt(index - 1) == password.charAt(index) ) {
             return false;
