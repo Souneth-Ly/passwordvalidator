8fdcd21e0e86a62b3dca5fd3974eb5152abadfac
uonsongly <uonsongly@gmail.com>
2018-02-18 21:53:13 -0700

    add unit tests

--- src/test/java/passwordvalidator/ValidatorTest.java
+++ src/test/java/passwordvalidator/ValidatorTest.java
@@ -1,4 +1,28 @@
 package passwordvalidator;
 
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Integration Test for Password Validation
+ */
 public class ValidatorTest {
+	private Validator validator;
+
+	@Before
+	public void setUp(){
+		validator = new Validator();
+	}
+
+	@Test
+	public void validPasswordTest() {
+		assertTrue( validator.isPasswordValid( "abc123" ) );
+	}
+
+	@Test
+	public void lessThan5CharactersTest()
+	{
+		assertFalse( validator.isPasswordValid( "abc1" ) );
+	}
 }
